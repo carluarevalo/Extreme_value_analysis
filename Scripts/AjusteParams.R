8051e588d64c7938b2e447c654efd7ff193f8a2a
Unknown <jacksparrow.88@gmail.com>
2020-08-05 20:23:13 -0300

    Agrego visualizacion para varios bloques

--- Scripts/AjusteParams.R
+++ Scripts/AjusteParams.R
@@ -12,38 +12,40 @@ boxplot(datos[,'tMax'], datos[,'tMin'])
 
 # Estimo params con Maxima verosimilitud sobre los datos
 # Ver consola para detalles:
-ajuste <- gev(datos[,'tMax'], 365)
-ajuste
-#$par.ests
-#xi      sigma         mu 
-#-0.2141458  6.0193876 20.9820117
-max_datos <- ajuste$data
-# Lo guardo en variables
-ests <- ajuste$par.ests
-xi <-ests[1]
-sigma <- ests[2]
-mu <- ests[3]
-# Los datos
-xgrid <- seq(0,100,0.1)
-# Histograma de TODOS los datos
-hist(max_datos, prob=T, col='yellowgreen', xlim=c(0, 50), ylim=c(0, 0.2))
-# Histograma de datos > 30 (por poner una referencia)
-#hist(datos[datos$tMax>35, 'tMax'], prob=T, col=rgb(1,0,0,0.5), add=T)
-# GEV con parametros encontrados arriba
-lines(xgrid, dgev(xgrid,
-                 xi = xi,
-                 mu = mu,
-                 sigma = sigma),
-     type='l', lwd=3, col='steelblue')
-# $par.ses en vez de $par.ests (no se que es)
-# lines(xgrid, dgev(xgrid,
-#                   xi = ajuste$par.ses[1],
-#                   mu = ajuste$par.ses[2],
-#                   sigma = ajuste$par.ses[3]),
-#       type='l', lwd=3, col='orange')
-# Linea vertical en xi
-abline(v=mu, lwd=3, lty=3, col='orange')
-legend('topleft', c('Datos','Ajuste GEV', 'mu_hat'),
-       lty=c(1,1,3), lwd=3,
-       col=c('yellowgreen', 'steelblue', 'orange'))
+for(i in c(1,2,3,4,5,6,7,14,30,60,90,120,240,365,730,3*365)){
+    ajuste <- gev(datos[,'tMax'], i)
+    ajuste
+    #$par.ests
+    #xi      sigma         mu 
+    #-0.2141458  6.0193876 20.9820117
+    max_datos <- ajuste$data
+    # Lo guardo en variables
+    ests <- ajuste$par.ests
+    xi <- ests[1]
+    sigma <- ests[2]
+    mu <- ests[3]
+    # Los datos
+    xgrid <- seq(0,100,0.1)
+    # Histograma de TODOS los datos
+    hist(max_datos, prob=T, col='yellowgreen', xlim=c(0, 50), ylim=c(0, 0.2), main=paste('Ajuste GEV datos\nBloque:', i, 'día/s'))
+    # Histograma de datos > 30 (por poner una referencia)
+    #hist(datos[datos$tMax>35, 'tMax'], prob=T, col=rgb(1,0,0,0.5), add=T)
+    # GEV con parametros encontrados arriba
+    lines(xgrid, dgev(xgrid,
+                     xi = xi,
+                     mu = mu,
+                     sigma = sigma),
+         type='l', lwd=3, col='steelblue')
+    # $par.ses en vez de $par.ests (no se que es)
+    # lines(xgrid, dgev(xgrid,
+    #                   xi = ajuste$par.ses[1],
+    #                   mu = ajuste$par.ses[2],
+    #                   sigma = ajuste$par.ses[3]),
+    #       type='l', lwd=3, col='orange')
+    # Linea vertical en xi
+    abline(v=mu, lwd=3, lty=3, col='orange')
+    legend('topleft', c(paste0('Datos (',length(max_datos),')'),'Ajuste GEV', 'mu_hat'),
+           lty=c(1,1,3), lwd=3,
+           col=c('yellowgreen', 'steelblue', 'orange'))
 
+}
