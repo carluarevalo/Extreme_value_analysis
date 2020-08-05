784cb243219aad87aefc26f8b2e884116213389b
Unknown <jacksparrow.88@gmail.com>
2020-08-05 11:30:28 -0300

    Corrijo un signo menos infiltrado y genearlizacion de params

--- Scripts/AjusteParams.R
+++ Scripts/AjusteParams.R
@@ -11,19 +11,22 @@ datos <- Algiers
 boxplot(datos[,'tMax'], datos[,'tMin'])
 
 # Estimo params con Maxima verosimilitud sobre los datos
-gev(datos[,'tMax'])
+gev(datos[,'tMax'], 100)
 #$par.ests
 #xi      sigma         mu 
 #-0.2141458  6.0193876 20.9820117 
-
+ests <- gev(datos[,'tMax'], 100)$par.ests
+xi <-ests[1]
+sigma <- ests[2]
+mu <- ests[3]
 # Los datos
-hist(datos[, 'tMax'], prob=T, col='yellowgreen')
+hist((datos[, 'tMax']-mean(datos[, 'tMax']))/sd(datos[, 'tMax']), prob=T, col='yellowgreen')
 # GEV con parametros encontrados arriba
 plot(seq(-100,100,0.1), dgev(-seq(-100,100,0.1),
-                             xi = -0.2141458,
-                             mu = 6.0193876,
-                             sigma = 20.9820117),
+                             xi = xi,
+                             mu = mu,
+                             sigma = sigma),
      type='l', lwd=3, col='steelblue')
 # Linea vertical en xi
-#abline(v=-0.2141458, lty=3, col='orange')
+abline(v=xi, lty=3, col='orange')
 
