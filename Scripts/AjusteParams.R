2db3530084e69976b78658be5de0d7b4ba1e1cb1
Unknown <jacksparrow.88@gmail.com>
2020-08-05 11:41:09 -0300

    Update de histograma + Ajuste anual

--- Scripts/AjusteParams.R
+++ Scripts/AjusteParams.R
@@ -11,21 +11,24 @@ datos <- Algiers
 boxplot(datos[,'tMax'], datos[,'tMin'])
 
 # Estimo params con Maxima verosimilitud sobre los datos
-gev(datos[,'tMax'], 100)
+gev(datos[,'tMax'], 365)
 #$par.ests
 #xi      sigma         mu 
 #-0.2141458  6.0193876 20.9820117 
-ests <- gev(datos[,'tMax'], 100)$par.ests
+ests <- gev(datos[,'tMax'], 365)$par.ests
 xi <-ests[1]
 sigma <- ests[2]
 mu <- ests[3]
 # Los datos
-hist((datos[, 'tMax']-mean(datos[, 'tMax']))/sd(datos[, 'tMax']), prob=T, col='yellowgreen')
+xgrid <- seq(-100,100,0.1)
+hist(datos[, 'tMax'], prob=T, col='yellowgreen', ylim=c(0, 0.2))
+hist(datos[datos$tMax>30, 'tMax'], prob=T, col=rgb(1,0,0,0.5), add=T)
 # GEV con parametros encontrados arriba
-plot(seq(-100,100,0.1), dgev(-seq(-100,100,0.1),
-                             xi = xi,
-                             mu = mu,
-                             sigma = sigma),
+
+lines(xgrid, dgev(xgrid,
+                 xi = xi,
+                 mu = mu,
+                 sigma = sigma),
      type='l', lwd=3, col='steelblue')
 # Linea vertical en xi
 abline(v=xi, lty=3, col='orange')
